1d0e2dfeb3d03df3596a544de7764b235e230a3c
Yichen Jiang <pauljyc0929@gmail.com>
2017-04-20 17:08:53 -0400

    add comment

--- wave_visualizer.py
+++ wave_visualizer.py
@@ -14,43 +14,44 @@ def main():
     #open a wav format music
     f = wave.open("snowy_mono.wav","r")
     timeMatrix = read("snowy_mono.wav")
-    # tmFile = open("timeMatrix.txt",'wb')
-    # numpy.savetxt(tmFile,tm[1])
-    # tmFile.close()
-
-    # plt.plot(timeMatrix[1])
-    # plt.show()
     signal = timeMatrix[1]
+
+    # Fourier Transform of the original signal
     freqMatrix = np.fft.fft(signal[chunk*0:chunk*5])
+
+    # Extract Magnitude and Phase plane from frequency plan
     magMatrix = np.absolute(freqMatrix)
     phaseMatrix = np.angle(freqMatrix)
 
+    # Inverse fourier transform back into time domain
     encodedSignal = np.fft.ifft(freqMatrix)
     encodedSignal = np.int16(encodedSignal)
     # plt.plot(encodedSignal)
     # plt.show()
+
+    # Cast encoded signal to a playable .wav file
     write("test.wav", 44100, encodedSignal)
 
-    # #instantiate PyAudio
-    # p = pyaudio.PyAudio()
-    # #open stream
-    # stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-    #             channels = f.getnchannels(),
-    #             rate = f.getframerate(),
-    #             output = True)
-    # #read data
-    # data = f.readframes(chunk)
-    #
-    # #play stream
-    # while data:
-    #     stream.write(data)
-    #     data = f.readframes(chunk)
-    #
-    # #stop stream
-    # stream.stop_stream()
-    # stream.close()
-    #
-    # #close PyAudio
+    #instantiate PyAudio
+    p = pyaudio.PyAudio()
+    #open stream
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                channels = f.getnchannels(),
+                rate = f.getframerate(),
+                output = True)
+    #read data
+    data = f.readframes(chunk)
+
+    #play stream
+    while data:
+        stream.write(data)
+        data = f.readframes(chunk)
+
+    #stop stream
+    stream.stop_stream()
+    stream.close()
+
+    #close PyAudio
     p.terminate()
 
 if __name__ == "__main__":
